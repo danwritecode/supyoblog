d384846e467a74ea63fb5045129ddc46d1b34aad
danwritecode <dnelson4993@outlook.com>
2023-07-05 12:44:56 -0400

    fixed mobile

--- components/article-card.vue
+++ components/article-card.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink :to="props.path">
-    <div class="grid grid-cols-2">
+    <div class="grid sm:grid-cols-2">
       <div class="bg-zinc-800 p-4">
         <h1 class="text-zinc-50 font-bold text-xl">{{ props.title }}</h1> 
         <p class="mt-2 text-zinc-300">{{ props.description }}</p>
@@ -10,7 +10,7 @@
         <img 
           :src="props.imgUrl" 
           alt="article-img"
-          class="h-56 w-full object-cover"
+          class="h-full sm:h-56 w-full object-cover"
         >
       </div>
     </div>
